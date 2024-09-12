a8af0fae40b9143581a3d362025283c23d401692
ishagupta <isha.gupta@menloschool.org>
2024-09-12 11:34:59 -0700

    First

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Highways & Hospitals
  * A puzzle created by Zach Blick
  * for Adventures in Algorithms
  * at Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Isha Gupta
  *
  */
 
@@ -15,6 +19,20 @@ public class HighwaysAndHospitals {
      *  hospital access for all citizens in Menlo County.
      */
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
+        boolean[] hasVisited = new boolean[n];
+        // Length = # of clusters = # hospitals
+        // Size of each element = number of cities --- # highways = cities - 1
+        ArrayList<Integer> clusters = new ArrayList<Integer>();
+
+
+        Queue<Integer> toVisit = new LinkedList<Integer>();
+        toVisit.add(0);
+
+
+        for(int i = 0; i < n; i++){
+        }
+
+
         return 0;
     }
 }
