0db682fd0a8b5378e3964aaafe76e47412585743
ishagupta <isha.gupta@menloschool.org>
2024-09-21 22:56:58 -0700

    passed 3 test cases

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -26,49 +26,67 @@ public class HighwaysAndHospitals {
         }
 
         int[] roots = new int[n+1];
+        int node = 0;
+        int temp = 0;
 
         int root1, root2 = 0;
         for(int i = 0; i < cities.length; i++){
             // Sets the root to the first layer above the city
-            root1 = roots[cities[i][0]];
-            root2 = roots[cities[i][1]];
+            root1 = cities[i][0];
+            root2 = cities[i][1];
+
+
 
             // Find root 1
-            // If the layer above is 0, then the city is a root (meaning city = city's root)
-            if(root1 == 0){
-                root1 = cities[i][0];
+            node = root1;
+            // Keep going up the tree until the level above you is zero(that's you're root)
+            while(roots[root1] > 0){
+                root1 = roots[root1];
             }
-            else{
-                // Keep going up the tree until the level above you is zero
-                while(roots[root1] != 0){
-                    root1 = roots[root1];
-                }
+            // Go through path again and assign everything on the way to top root
+            // While the root of the node is not the top off the tree **and** the root of the node is not 0
+            while(roots[node] > 0 && roots[node] != root1 ){
+                temp = roots[node];
+                roots[node] = root1;
+                node = temp;
             }
 
+
+
             // Similarly, find root 2
-            if(root2 == 0){
-                root2 = cities[i][1];
+            // Keep going up the tree until the level above you is zero
+            node = root2;
+            while(roots[root2] > 0){
+                root2 = roots[root2];
             }
-            else{
-                // Keep going up the tree until the level above you is zero
-                while(roots[root2] != 0){
-                    root2 = roots[root2];
-                }
+            // Go through path again and assign everything on the way to top root
+            while(roots[node] > 0 && roots[node] != root2){
+                temp = roots[root1];
+                roots[node] = root2;
+                node = temp;
             }
 
 
             if(root1 != root2){
-                // Make root1 the root of root2
-                //roots[root2] = root1;
-                roots[root2] = cities[i][0];
-            }
+                // Make the root with smaller magnitude of order the root
+                if(roots[root2] >= roots[root1]) {
+                    // Make root1 the root of root2
+                    roots[root1] = roots[root2] - 1;
+                    roots[root2] = cities[i][0];
+                }
+                else{
+                    // Make root2 the root of root1
+                    roots[root2] = roots[root1] - 1;
+                    roots[root1] = cities[i][1];
 
+                }
+            }
         }
 
         // Figure out how many cluster there are by looking at number of empty element in array
         int clusters = 0;
         for(int i = 1; i < roots.length; i++){
-            if (roots[i] == 0){
+            if (roots[i] < 0){
                 clusters++;
             }
         }
