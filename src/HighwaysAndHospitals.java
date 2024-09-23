76603235b225623784c24c2f84b4a2c9f51e0e65
ishagupta <isha.gupta@menloschool.org>
2024-09-22 18:10:06 -0700

    Final

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -20,8 +20,7 @@ public class HighwaysAndHospitals {
      *  hospital access for all citizens in Menlo County.
      */
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
-        // # empty slots = # of clusters = # hospitals
-        // # highways = cities(n) - # of clusters(empty slots)
+        // If highways are more expensive than hospitals, then build hospitals everywhere
         if(highwayCost > hospitalCost){
             System.out.println((long)(hospitalCost)* (long)(n));
             return (long)(hospitalCost)* (long)(n);
@@ -30,45 +29,43 @@ public class HighwaysAndHospitals {
         int[] roots = new int[n+1];
         int node = 0;
         int temp = 0;
-
         int root1, root2 = 0;
+
         for(int i = 0; i < cities.length; i++){
-            // Sets the root to the first layer above the city
+            // Temporarily sets the roots to the cities we are trying to merge
             root1 = cities[i][0];
             root2 = cities[i][1];
 
-
             // Find root 1
             node = root1;
-            // Keep going up the tree until the level above you is zero(that's you're root)
+            // Keep going up the tree until the level above you is a root (ie negative)
             while(roots[root1] > 0){
                 root1 = roots[root1];
             }
             // Go through path again and assign everything on the way to top root
-            // While the root of the node is not the top off the tree **and** the root of the node is not 0
-            while(roots[node] > 0 && roots[node] != root1 ){
+            while(roots[node] > 0){
                 temp = roots[node];
                 roots[node] = root1;
                 node = temp;
             }
 
             // Similarly, find root 2
-            // Keep going up the tree until the level above you is zero
+            // Keep going up the tree until the level above you is a root (ie negative)
             node = root2;
             while(roots[root2] > 0){
                 root2 = roots[root2];
             }
             // Go through path again and assign everything on the way to top root
-            while(roots[node] > 0 && roots[node] != root2){
+            while(roots[node] > 0){
                 temp = roots[node];
                 roots[node] = root2;
                 node = temp;
             }
 
-
+            // Combine trees
             if(root1 != root2){
                 // Make the root with smaller magnitude of order the root
-                if(roots[root2] >= roots[root1]) {
+                if(roots[root2] >= roots[root1]){
                     // Make root1 the root of root2
                     roots[root1] = roots[root2] - 1;
                     roots[root2] = cities[i][0];
@@ -82,18 +79,26 @@ public class HighwaysAndHospitals {
             }
         }
 
-        // Figure out how many cluster there are by looking at number of empty element in array
+
+        // Do math to figure out final cost
         int clusters = 0;
         int singles = 0;
+        // Start for loop at one to avoid counting roots[0], which is always empty
         for(int i = 1; i < roots.length; i++){
+            // Figure out how many cluster there are by looking at number of negative elements in array
             if (roots[i] < 0){
                 clusters++;
             }
+            // Figure out how many single cities there are by looking at number of 0s in array
             if (roots[i] == 0){
                 singles++;
             }
         }
         System.out.println((long)(hospitalCost * (clusters+singles)) + (long)highwayCost*(n - (singles + clusters)));
-        return (long)(hospitalCost * (long)(clusters+singles)) + (long)highwayCost*((long)n - (long)(singles + clusters));
+        // Put hospitals at all stand-alone cities and put one hospital at each cluster
+        // Put highways between clusters = (n - # of clusters)
+        // But also no highways at stand-alone cities (so subtract singles)
+        return (hospitalCost * (long)(clusters+singles))
+                + (long)highwayCost*((long)n - (long)(singles + clusters));
     }
 }
